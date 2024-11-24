a502b39dddaab8039d527d04c0fe8a0d12000117
lanmei699 <1826132537@qq.com>
2024-11-24 23:54:57 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+current_dir=$(basename "$PWD")
+
+    cd ..
+    rm -rf "$current_dir"
+    
+    git clone https://github.com/lanmei699/BlueBerryTOOL
+    cd
+    cd 蓝莓工具
+    bash req.sh
+    chmod +x 蓝莓工具
+    ./蓝莓工具
+else
+    echo "取消。"
+fi
